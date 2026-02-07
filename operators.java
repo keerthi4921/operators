3d925e3c8c4238b846c3c7debf517df88864f199
keerthi4921 <keerthia729@gmail.com>
2026-02-07 14:58:44 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,14 @@
+public class operators {
+    public static void main(String[] args) {
+        int a=3;
+        if(a>0&&a%2==0){
+            System.out.println("given number is even and positive");
+
+        }
+        else{
+            System.out.println("not a positive and even number");
+
+        }
+    }
+    
+}
