354f7a079f9a8f93fdb121b662f776d22c0c4887
keerthi4921 <keerthia729@gmail.com>
2026-02-07 14:19:54 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,12 @@
+public class arithmetic {
+    public static void main(String[] args) {
+        int a=5;
+        int b=9;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+    
+}
